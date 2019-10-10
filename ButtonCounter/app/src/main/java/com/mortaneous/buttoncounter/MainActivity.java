cb0bfbb073ee685c02b2efe1e743f355096e864c
Mortaneous <mortaneous.git@gmail.com>
2019-10-11 02:47:19 +0800

    ButtonCounter App refinements

--- ButtonCounter/app/src/main/java/com/mortaneous/buttoncounter/MainActivity.java
+++ ButtonCounter/app/src/main/java/com/mortaneous/buttoncounter/MainActivity.java
@@ -5,6 +5,7 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 import android.text.method.ScrollingMovementMethod;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
@@ -12,47 +13,58 @@ import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
-    private static final String TEXT_VIEW_KEY = "textViewContents";
+    private final String TAG = "MainActivity";
+    private final String TEXT_VIEW_KEY = "textViewContents";
     private EditText userInput;
     private Button button;
     private TextView textView;
-    private int clickCount = 0;
 
     @Override
     protected void onCreate(final Bundle savedInstanceState) {
+        Log.d(TAG, "onCreate: start");
+
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        userInput = (EditText) findViewById(R.id.editText);
+        userInput = findViewById(R.id.editText);
         userInput.setText("");
 
-        textView = (TextView) findViewById(R.id.textView);
+        textView = findViewById(R.id.textView);
         textView.setText("");
-        if(savedInstanceState != null) {
-            CharSequence textViewContents = savedInstanceState.getCharSequence(TEXT_VIEW_KEY);
-            if (textViewContents != null) {
-                textView.setText(textViewContents.toString());
-            }
-        }
 
         textView.setMovementMethod(new ScrollingMovementMethod());
 
-        button = (Button) findViewById(R.id.button);
+        button = findViewById(R.id.button);
         button.setText("Add");
         button.setOnClickListener((v) -> {
                 String input = userInput.getText().toString();
                 if(!input.isEmpty()) {
-                    textView.append(input.toString() + "\n");
+                    textView.append(input + "\n");
                     userInput.setText("");
 
                 }
         });
+
+        Log.d(TAG, "onCreate: end");
     }
 
     @Override
     protected void onSaveInstanceState(@NonNull Bundle outState) {
-        super.onSaveInstanceState(outState);
+        Log.d(TAG, "onSaveInstanceState: start");
 
+        super.onSaveInstanceState(outState);
         outState.putCharSequence(TEXT_VIEW_KEY, textView.getText());
+
+        Log.d(TAG, "onSaveInstanceState: end");
+    }
+
+    @Override
+    protected void onRestoreInstanceState(@NonNull Bundle savedInstanceState) {
+        Log.d(TAG, "onRestoreInstanceState: start");
+
+        super.onRestoreInstanceState(savedInstanceState);
+        textView.setText(savedInstanceState.getCharSequence(TEXT_VIEW_KEY).toString());
+
+        Log.d(TAG, "onRestoreInstanceState: end");
     }
 }
